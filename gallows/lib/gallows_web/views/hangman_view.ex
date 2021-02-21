f213767c7eb23b117c5887668ca5aae1a42cc99c
Lars Bünting <lars.buenting@smacc.io>
2021-02-21 20:54:24 +0100

    phx app 3/5

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,13 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  import GallowsWeb.Helpers.GameStateHelper
+
+  def new_game_button(conn) do
+    button("New Game", to: Routes.hangman_path(conn, :create_game))
+  end
+
+  def game_over?(%{game_state: game_state}) do
+    game_state in [:won, :lost]
+  end
 end
